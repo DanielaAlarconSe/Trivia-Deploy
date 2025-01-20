4ecdd570431ca4f644b123002fd8075cb49ca9c7
DanielaAlarconSe <danielaalarconsepulveda30@gmail.com>
2025-01-20 16:40:36 -0500

    update principal por public

--- src/main/java/com/ciber/daoImpl/PersonaDaoImpl.java
+++ src/main/java/com/ciber/daoImpl/PersonaDaoImpl.java
@@ -46,7 +46,7 @@ public class PersonaDaoImpl implements IPersonaDao {
 
 	@Override
 	public int registrar(Persona persona) {
-		String sql = " INSERT INTO principal.persona " + " (per_apellido, per_nombre, per_pais_residencia, per_email) "
+		String sql = " INSERT INTO public.persona " + " (per_apellido, per_nombre, per_pais_residencia, per_email) "
 				+ " VALUES(?, ?, ?, ?); ";
 
 		int result = jdbcTemplateEjecucion.update(sql, new Object[] { persona.getApellido(), persona.getNombre(),
@@ -74,7 +74,7 @@ public class PersonaDaoImpl implements IPersonaDao {
 	public int actualizar(Persona persona) {
 		System.out.println(persona);
 
-		String sql = " UPDATE principal.persona "
+		String sql = " UPDATE public.persona "
 				+ " SET per_apellido = ?, per_nombre = ?, per_pais_residencia = ?, per_email = ?  "
 				+ " WHERE per_codigo = ?; ";
 
@@ -102,10 +102,10 @@ public class PersonaDaoImpl implements IPersonaDao {
 	public List<PersonaDto> obtenerPersonasUsuario() {
 
 		String sql = "select p.per_codigo, p.per_nombre, p.per_apellido, p.per_email, p.per_pais_residencia, p.per_fecha_registro, "
-				+ "u.uwd2 , ut.ust_codigo , ut.ust_nombre, count(case when u.usu_estado = 1 then 1 else null end) as usuario, e.ent_codigo, e.ent_nombre from principal.persona p "
-				+ "left join principal.usuario u on p.per_codigo = u.per_codigo "
-				+ "left join principal.usuario_tipo ut on u.ust_codigo = ut.ust_codigo "
-				+ "left join principal.entidad e on u.ent_codigo = e.ent_codigo "
+				+ "u.uwd2 , ut.ust_codigo , ut.ust_nombre, count(case when u.usu_estado = 1 then 1 else null end) as usuario, e.ent_codigo, e.ent_nombre from public.persona p "
+				+ "left join public.usuario u on p.per_codigo = u.per_codigo "
+				+ "left join public.usuario_tipo ut on u.ust_codigo = ut.ust_codigo "
+				+ "left join public.entidad e on u.ent_codigo = e.ent_codigo "
 				+ "where p.per_estado = 1 group by p.per_codigo, p.per_nombre, p.per_apellido, p.per_email, "
 				+ "p.per_pais_residencia, p.per_fecha_registro, u.uwd2 , ut.ust_codigo , ut.ust_nombre, e.ent_codigo, e.ent_nombre order by usuario desc;";
 
@@ -115,10 +115,10 @@ public class PersonaDaoImpl implements IPersonaDao {
 	@Override
 	public List<PersonaDto> obtenerAspirantesEntidad(Integer entidad) {
 
-		String sql = "select *, u.usu_codigo as usuario from principal.usuario u "
-				+ "inner join principal.usuario_tipo ut on u.ust_codigo  = ut.ust_codigo "
-				+ "inner join principal.persona p on u.per_codigo = p.per_codigo "
-				+ "left join principal.entidad e on u.ent_codigo = e.ent_codigo "
+		String sql = "select *, u.usu_codigo as usuario from public.usuario u "
+				+ "inner join public.usuario_tipo ut on u.ust_codigo  = ut.ust_codigo "
+				+ "inner join public.persona p on u.per_codigo = p.per_codigo "
+				+ "left join public.entidad e on u.ent_codigo = e.ent_codigo "
 				+ "where u.ent_codigo = ? and u.usu_estado = 1 and p.per_estado = 1";
 
 		return jdbcTemplate.query(sql, new PersonaDtoSetExtractor(), entidad);
@@ -126,8 +126,8 @@ public class PersonaDaoImpl implements IPersonaDao {
 
 	@Override
 	public List<Persona> obtenerInstructores() {
-		String sql = " SELECT p.per_codigo , p.per_apellido, p.per_nombre FROM principal.persona p "
-				+ " INNER JOIN principal.usuario u ON p.per_codigo = u.per_codigo "
+		String sql = " SELECT p.per_codigo , p.per_apellido, p.per_nombre FROM public.persona p "
+				+ " INNER JOIN public.usuario u ON p.per_codigo = u.per_codigo "
 				+ " WHERE u.ust_codigo = 2 ORDER BY p.per_apellido , p.per_nombre DESC ";
 		return jdbcTemplate.query(sql, new InstructorSetExtractor());
 	}
